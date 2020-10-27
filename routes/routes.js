0552764051e66bd06bd310f63bd7b1f61e8572be
Alexandru Ivascu <ivascua@coventry.ac.uk>
2020-10-27 15:36:40 +0000

    New page, TODO put boxes for new event info

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,11 @@ import Router from 'koa-router'
 
 import publicRouter from './public.js'
 import secureRouter from'./secure.js'
+import infoRouter from './newevent.js'
 
 const mainRouter = new Router()
 
-const nestedRoutes = [publicRouter, secureRouter]
+const nestedRoutes = [publicRouter, secureRouter, infoRouter]
 for (const router of nestedRoutes) {
 	mainRouter.use(router.routes())
 	mainRouter.use(router.allowedMethods())

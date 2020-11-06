732f4750415251ac20bf1cc09713f5dd2b977572
Alexandru Ivascu <ivascua@coventry.ac.uk>
2020-11-06 14:57:55 +0000

    trying to figure how to display items nicely

--- routes/public.js
+++ routes/public.js
@@ -3,8 +3,13 @@ import Router from 'koa-router'
 import bodyParser from 'koa-body'
 
 import Contacts from '../modules/contacts.js'
+import data from '../modules/contacts.js'
 const dbName = 'website.db'
 import Accounts from '../modules/accounts.js'
+
+import sqlite from 'sqlite-async'
+import mime from 'mime-types'
+import fs from 'fs-extra'
 
 const router = new Router()
 router.use(bodyParser({multipart: true}))
@@ -21,13 +26,17 @@ router.get('/', async ctx => {
 	try {
 		const records = await contacts.all()
 		console.log(records)
-		ctx.hbs.records = records
-		await ctx.render('index', ctx.hbs)
+    /*ctx.hbs.records.forEach(element => {
+    let itemName = element.items.split("~")[0];
+    let itemPrice = ctx.hbs.records[0].items.split("~")[1];
+    let itemLink = ctx.hbs.records[0].items.split("~")[2];
+    });*/
+    ctx.hbs.records = records
+    await ctx.render('index', ctx.hbs)
 	} catch(err) {
 		await ctx.render('error', ctx.hbs)
 	}
 })
-
 
 /**
  * The user registration page.

6991da2ac40c0deb7cf99600d3e9e039e80f942a
Alexandru Ivascu <ivascua@coventry.ac.uk>
2020-11-26 21:53:44 +0000

    TODO check on main.js

--- public/main.js
+++ public/main.js
@@ -8,5 +8,29 @@ window.addEventListener('DOMContentLoaded', () => {
 	window.setTimeout( () => {
 		document.querySelector('aside').hidden = true
 	}, delay)
-})
+}
 
+if(document.querySelector('button.back')) {
+  document.querySelectorAll('button.back').forEach(element) => {
+    element.addEventListener('click',() => {
+      console.log('Back button clicked')
+      console.log(window.history)
+      window.history.back()
+    })
+  }
+}
+
+if(document.querySelector('input')) {
+  document.querySelectorAll('input').forEach(element => {
+    element.addEventListener('invalid',event => {
+      if(!event.target.validity.valid) {
+        const msg = event.dataset.target.msg || 'Invalid data'
+        event.target.setCustomValidity(msg)
+      }
+    })
+    element.addEventListener('input', event => {
+      event.target.setCustomValidity('')
+    })
+  }, false)
+}
+})

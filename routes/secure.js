8d764f168163c1d7994619c4e2059e324ffae10f
Alexandru Ivascu <ivascua@coventry.ac.uk>
2020-12-01 16:36:10 +0000

    merging cause this is boring

--- routes/secure.js
+++ routes/secure.js
@@ -18,7 +18,6 @@ router.use(checkAuth)
 router.get('/', async ctx => {
 	const contacts = await new Contacts(dbName)
 	try {
-		console.log('THIS IS A TEST')
 		const records = await contacts.all()
 		console.log(records)
 		ctx.hbs.records = records
